dc16d95352211c10db801cae408639ebe5854045
casingalaaron <casingalaaron04@gmail.com>
2026-07-03 08:39:47 +0800

    redesign User interface
    - Added a button instead of prompt input
    
    - replace a button event decision instead of function

--- app.js
+++ app.js
@@ -20,44 +20,16 @@ function getComputerChoice(){
     return stringChoice
 }
 
-function getHumanChoice(){
-    let choice = ""
-    const buttons = document.querySelectorAll('button')
-
-    buttons.forEach((button) => {
-        button.addEventListener("click", (event) => {
-            const buttonClicked = event.target.id;
-            console.log(buttonClicked)
-            if(buttonClicked === "rockBTN"){
-                choice = "rock"
-            }
-            else if(buttonClicked === "paperBTN"){
-                choice = "paper"
-            }
-            else{
-                choice = "scissor"
-            }
-        })
-    })
-    return choice
-}
-
-
-
-
-
 function playRound(HumanChoice, ComputerChoice){
 
     const Attacks = [HumanChoice, ComputerChoice]
-    let humanScore = 0
-    let computerScore = 0
     let roundWinner = 2
     let grandWinner = 0
     
     if(Attacks.includes("rock") && Attacks.includes("scissor")){
         grandWinner = Attacks.indexOf("rock")
         console.log(`${grandWinner}`)
-        if(grandWinner == 0){
+        if(roundWinner == 0){
             humanScore++
             alert(`Human Won - ${HumanChoice} over ${ComputerChoice}`)
         }
@@ -69,7 +41,7 @@ function playRound(HumanChoice, ComputerChoice){
     else if(Attacks.includes("paper") && Attacks.includes("rock")){
         grandWinner = Attacks.indexOf("paper")
         console.log(`${grandWinner}`)
-        if(grandWinner == 0){
+        if(roundWinner == 0){
             humanScore++
             alert(`Human Won - ${HumanChoice} over ${ComputerChoice}`)
         }
@@ -81,7 +53,7 @@ function playRound(HumanChoice, ComputerChoice){
     else if(Attacks.includes("scissor") && Attacks.includes("paper")){
         grandWinner = Attacks.indexOf("scissor")
         console.log(`${grandWinner}`)
-        if(grandWinner == 0){
+        if(roundWinner == 0){
             humanScore++
             alert(`Human Won - ${HumanChoice} over ${ComputerChoice}`)
         }
@@ -96,13 +68,37 @@ function playRound(HumanChoice, ComputerChoice){
     else{
         alert("Invalid Attack")
     }
+    
     grandWinner = humanScore > computerScore ? "Human Won" : "Computer Won"
     const result = document.querySelector('#result')
-    result.textContent = `Human score: ${humanScore}\nComputer score: ${computerScore}`
+    result.innerHTML = `Human score: ${humanScore}\n\nComputer score: ${computerScore}`
 }
-let human = getHumanChoice()
-let computer = getComputerChoice()
-playRound(human, computer)
 
 
-alert(`Human score: ${humanScore}\nComputer score: ${computerScore}`)
+let humanChoice = ""
+let computerChoice = getComputerChoice()
+let humanScore = 0
+let computerScore = 0
+
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const buttonClicked = event.target.id;
+
+        if(buttonClicked === "rockBTN"){
+            humanChoice = "rock"
+        }
+        else if(buttonClicked === "paperBTN"){
+            humanChoice === "paper"
+        }
+        else{
+            humanChoice === "scissor"
+        }
+
+        playRound(humanChoice, computerChoice)
+        alert(`Human score: ${humanScore}\nComputer score: ${computerScore}`)
+        console.log(`Human Choice: ${humanChoice}`)
+    })
+})
+
